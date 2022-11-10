bf75d347d8fd17c5dbaba94d35b8594914f75395
Mac7 <mac7@Mac7s-iMac.local>
2022-11-10 20:52:05 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+//var data = 234
+// let data1 = 345
+// const data2 = 'hfuierqhfie'
+// //number
+// //string
+// //null
+// //undefined
+// console.log('Hello world by JS', data1)
+// console.log(data2)
+// //Math
+// let number1 = 45
+// let number2 = 85
+// let result = number1 / number2
+// console.log('your result = ', result)
+// //Allert
+
+// alert('Hello, good luck')
+// let your_age = prompt('How old are you?')
+
+
+
+// if(1 > 2){
+//     console.log('1 is more than 2') 
+// }else {
+//     console.log('2 is more than 1')
+// }
+
+// if(3 > 2){
+//     console.log('3 is more than 2') 
+// }else {
+//     console.log('2 is more than 1')
+// }
+
+// let min_age = 18
+// if(your_age > min_age) {
+//     console.log ('Yes, you can enter')
+// }else {
+//     console.log('please leave the site')
+// }
+
+// let user = prompt('please your name')
+// if(user == 'aziz') {
+//     console.log('yes you are admin')
+// }else {
+//     console.log('no you are not admin')
+// }
+
+// //Math
+// let number1 = 12
+// let number2 = 23
+// let number3 = 34
+// let number4 = 45
+// let result = number1 - number2 + number3 * number4 / 3
+// console.log('12-23+34*45/3 =', result )
+// let result2 = number1 - (number2 + number3) * number4 / 3
+// console.log('12 - (23+34) * 45 /3 =', result2 )
+
+// // age
+// let min_age = 20 
+// let your_age = prompt('Please your age')
+// if(your_age < 20){
+//     console.log('go away')
+// }else {
+//     console.log('come here')
+// }
+// //js
+// let language = prompt('Chei progrmist?')
+// if(language == 'Mr.JS') {
+//     console.log('Good, idi k nam')
+// }else {
+//     console.log('Ploho, idi k python')
+// }
+
+// let password = prompt('your password')
+// let login = prompt('your name')
+// if(password == '12345' || password == '1234' && login == 'admin'){
+//     console.log('Correct')
+// }else {
+//     console.log('Not correct')
+// }
+
+// let num1 = 1
+// let num2 = '1'
+// console.log('hello world', num1 + num2)
+// console.log('hello world', '5' + 7)
+// console.log('hello world', '5 apples' + ' ' +5 )
+
+
+// let home = prompt('Home Adress')
+// let number = prompt('phone number')
+// let correctNumber = 12345
+// if(home == 'Oltintepa' || home == 'oltintepa' && Number(number) === correctNumber){
+//     console.log('Correct Adress')
+// }else {
+//     console.log('Try again')
+// }
+
+for(let i = 0; i < 10;i++){
+    console.log(i)
+}
+
+for(let x = 0; x )
